36efd23040b752f07012044d2635a775ac3d4eae
Liam Cordelle <liamcordelle1234@gmail.com>
2018-06-07 20:27:06 +1200

    Add API for temperature - let's get stateful

--- src/components/TemperatureFooter.js
+++ src/components/TemperatureFooter.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react';
 import './ComponentCss/TemperatureFooter.css';
 
 class TemperatureFooter extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {temperature: this.getLatestTemperature()};
+  }
+
+  getLatestTemperature() {
+    fetch('/getTemperature').then(response => {
+      return response.json();
+    }).then(data => {
+      this.setState({temperature: data});
+    })
+  }
+
+  componentDidMount() {
+    this.temperatureRefresher = setInterval(
+      () => this.getLatestTemperature(),
+      5000
+    );
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.temperatureRefresher);
+  }
+
   render() {
     return (
-      <div class="TemperatureFooter">
-        <div class="centering-table">
-          <p class="centering-table-cell">The temperature is TEMPERATURE_PLACEHOLDER&deg;C</p>
+      <div className="TemperatureFooter">
+        <div className="centering-table">
+          <p className="centering-table-cell">The temperature is {this.state.temperature}&deg;C</p>
         </div>
       </div>
     )
