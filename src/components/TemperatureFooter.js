a7e5a49eeb0158aa954f42a3403be9fa9deea243
Liam Cordelle <liamcordelle1234@gmail.com>
2021-06-27 13:53:10 +1200

    Reduce temperature polling rate

--- src/components/TemperatureFooter.js
+++ src/components/TemperatureFooter.js
@@ -24,7 +24,7 @@ class TemperatureFooter extends Component {
   componentDidMount() {
     this.temperatureRefresher = setInterval(
       () => this.getLatestTemperature(),
-      5000
+      30000
     );
   }
 
